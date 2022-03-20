318395124303e9e1fbda603f66a67eb87cf2966d
小野裕輔 <y-ono@nswt.co.jp>
2022-03-20 17:38:34 +0900

    3.2 画面から別画面に値を渡すまで

--- example/src/main/java/work/y_ono/example/hello/HelloController.java
+++ example/src/main/java/work/y_ono/example/hello/HelloController.java
@@ -1,7 +1,10 @@
 package work.y_ono.example.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -9,5 +12,13 @@ public class HelloController {
     public String getHello() {
         // hello.htmlに画面遷移
         return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String postRequest(@RequestParam("text1") String str, Model model){
+        model.addAttribute("sample", str);
+
+        // response.htmlに画面遷移
+        return "hello/response";
     }
 }
