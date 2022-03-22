8f93f15a6f788db4b58cc7c3b1aba242c35c4d66
小野裕輔 <y-ono@nswt.co.jp>
2022-03-23 08:20:16 +0900

    Remove lombok's getter/setter

--- example/src/main/java/work/y_ono/example/hello/Employee.java
+++ example/src/main/java/work/y_ono/example/hello/Employee.java
@@ -7,22 +7,4 @@ public class Employee {
     private String employeeId;
     private String employeeName;
     private int employeeAge;
-    public String getEmployeeId() {
-        return employeeId;
-    }
-    public int getEmployeeAge() {
-        return employeeAge;
-    }
-    public void setEmployeeAge(int employeeAge) {
-        this.employeeAge = employeeAge;
-    }
-    public String getEmployeeName() {
-        return employeeName;
-    }
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
 }
