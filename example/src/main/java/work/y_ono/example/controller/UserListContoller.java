5743705a702bf5e9ce14c0c9a24dbc117cc5dfaf
小野裕輔 <y-ono@nswt.co.jp>
2022-03-27 14:23:05 +0900

    8.3.1 動的SQLまで

--- example/src/main/java/work/y_ono/example/controller/UserListContoller.java
+++ example/src/main/java/work/y_ono/example/controller/UserListContoller.java
@@ -2,14 +2,18 @@ package work.y_ono.example.controller;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import work.y_ono.example.domain.user.model.MUser;
 import work.y_ono.example.domain.user.service.UserService;
+import work.y_ono.example.form.UserListForm;
 
 @Controller
 @RequestMapping("/user")
@@ -18,11 +22,37 @@ public class UserListContoller {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     // ユーザー一覧画面を表示
     @GetMapping("/list")
-    public String getUserList(Model model) {
+    public String getUserList(@ModelAttribute UserListForm form,
+            Model model) {
+
+        // formをMUserクラスに変換
+        MUser user = modelMapper.map(form, MUser.class);
+
         // ユーザー一覧取得
-        List<MUser> userList = userService.getUsers();
+        List<MUser> userList = userService.getUsers(user);
+
+        // Modelに登録
+        model.addAttribute("userList", userList);
+
+        // ユーザー一覧画面を表示
+        return "user/list";
+    }
+
+    // ユーザー検索処理
+    @PostMapping("/list")
+    public String postUserList(@ModelAttribute UserListForm form,
+            Model model) {
+
+        // formをMUserクラスに変換
+        MUser user = modelMapper.map(form, MUser.class);
+        
+        // ユーザー検索
+        List<MUser> userList = userService.getUsers(user);
 
         // Modelに登録
         model.addAttribute("userList", userList);
--- example/src/main/java/work/y_ono/example/form/UserListForm.java
+++ example/src/main/java/work/y_ono/example/form/UserListForm.java
@@ -1,5 +1,9 @@
 package work.y_ono.example.form;
 
+import lombok.Data;
+
+@Data
 public class UserListForm {
-    
+    private String userId;
+    private String userName;
 }
