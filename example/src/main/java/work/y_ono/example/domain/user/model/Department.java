9ce4c2fd878bd4acfb8989159332546bd3d91ef7
小野裕輔 <y-ono@nswt.co.jp>
2022-03-27 14:24:51 +0900

    8.3.2 ネストしたオブジェクトへのマッピング

--- example/src/main/java/work/y_ono/example/domain/user/model/Department.java
+++ example/src/main/java/work/y_ono/example/domain/user/model/Department.java
@@ -1,5 +1,9 @@
 package work.y_ono.example.domain.user.model;
 
+import lombok.Data;
+
+@Data
 public class Department {
-    
+    private Integer departmentId;
+    private String departmentName;
 }
