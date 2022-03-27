211b6d2ba72e8bcaa9e1f0b95c74a70b16643ec5
小野裕輔 <y-ono@nswt.co.jp>
2022-03-28 07:56:09 +0900

    10.2.2 コントローラークラスごとの例外処理まで

--- example/src/main/java/work/y_ono/example/controller/SignupController.java
+++ example/src/main/java/work/y_ono/example/controller/SignupController.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,14 +52,14 @@ public class SignupController {
     // ユーザー登録処理
     @PostMapping("/signup")
     public String postSignup(Model model, Locale locale,
-        @ModelAttribute @Validated(GroupOrder.class) SignupForm form,
-        BindingResult bindingResult) {
+            @ModelAttribute @Validated(GroupOrder.class) SignupForm form,
+            BindingResult bindingResult) {
         // 入力チェック
         if (bindingResult.hasErrors()) {
             // NG:ユーザー登録画面に戻る
             return getSignup(model, locale, form);
         }
-        
+
         log.info(form.toString());
 
         // formをMUserクラスに変換
@@ -67,5 +70,35 @@ public class SignupController {
 
         // ログイン画面にリダイレクト
         return "redirect:/login";
+    }
+
+    // データベース関連の例外処理
+    @ExceptionHandler(DataAccessException.class)
+    public String dataAccessExceptHandler(DataAccessException e, Model model) {
+        // 空文字をセット
+        model.addAttribute("error", "");
+
+        // メッセージをModelに登録
+        model.addAttribute("message", "SignupControllerで例外が発生しました。");
+
+        // HTTPのエラーコード（500）をModelに登録
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return "error";
+    }
+
+    // その他の例外処理
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandler(Exception e, Model model) {
+        // 空文字をセット
+        model.addAttribute("error", "");
+
+        // メッセージをModelに登録
+        model.addAttribute("message", "SignupControllerで例外が発生しました");
+
+        // HTTPのエラーコード（500）をModelに登録
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return "error";
     }
 }
